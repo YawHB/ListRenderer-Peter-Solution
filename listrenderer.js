6f84cffca56e6579c426e5c09a7ebac76baffb3d
Peter Lind (KEA) <petl@kea.dk>
2023-09-30 16:06:59 +0200

    del 3 - punkt 8 - sortering kan skifte retning

--- listrenderer.js
+++ listrenderer.js
@@ -4,25 +4,46 @@ export function construct(list, container, itemRenderer) {
     itemRenderer: itemRenderer,
     render() {
       this.container.innerHTML = "";
-      for(const item of list) {
+      for (const item of list) {
         const html = this.itemRenderer.render(item);
         this.container.insertAdjacentHTML("beforeend", html);
       }
     },
-    sort( sortBy, sortDir ) {
+    sort(sortBy, sortDir) {
       this.sortBy = sortBy;
 
-      list.sort((a,b) => {
-        if( a[this.sortBy] > b[this.sortBy]) {
-          return 1;
+      if (sortDir !== undefined) {
+        this.sortDir = sortDir;
+      } else {
+        // hvis this.sortDir var "asc", så sæt den til "desc" - eller omvendt
+        if (this.sortDir === "asc") {
+          this.sortDir = "desc";
         } else {
-          return -1;
+          this.sortDir = "asc";
+        }
+      }
+
+      console.log(`Sorter efter ${this.sortBy} i retning ${this.sortDir}`);
+
+      list.sort((a, b) => {
+        if (this.sortDir === "asc") {
+          if (a[this.sortBy] > b[this.sortBy]) {
+            return 1;
+          } else {
+            return -1;
+          }
+        } else {
+          if (a[this.sortBy] < b[this.sortBy]) {
+            return 1;
+          } else {
+            return -1;
+          }
         }
       });
 
       this.render();
-    }
-  }
+    },
+  };
 
   return ListRenderer;
 }
